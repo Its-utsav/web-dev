dbf2a591012637d66d1050807bec91c6f4519ec5
Its-utsav <utsavdhimmar987@gmail.com>
2024-04-06 12:33:19 +0530

    long time commit , and start self pratice project

--- react/react-by-HT/06-currency-convertor/src/hooks/useCurrencyInfo.js
+++ react/react-by-HT/06-currency-convertor/src/hooks/useCurrencyInfo.js
@@ -5,13 +5,14 @@ function useCurrencyInfo(currency) {
   // when we change currency the data need to change in UI that reason we use useState
   // useEffect run when website load and any change in currency (see dependency array);
   useEffect(() => {
-    let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
+    let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+  
     fetch(url) // fetch data
       .then((res) => res.json()) // data convert into json
       .then((res) => setData(res[currency])); // call setData function with current currency
-    // console.table(data);
+   
   }, [currency]); // dependency array
-  // console.log(data);
+
   return data; 
 }
 
