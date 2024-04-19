e5ce7f82669a3942645a037d66120ebfcc27d887
Its-utsav <utsavdhimmar987@gmail.com>
2024-04-19 12:22:29 +0530

    made so may changes

--- tailwind-css/lecture-0/intro.md
+++
@@ -1,4 +0,0 @@
-# what is tailwind ?
-
-- it is a **utility - first CSS- Framework** , it provide a collection of low level , single purpose css .
-- it makes writing css easy .
--- tailwind-css/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["lecture-0/dst/*.html"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
