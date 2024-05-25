26af52aea502907d1d10df8c730b0f8e5b8f270b
Its-utsav <utsavdhimmar987@gmail.com>
2024-05-25 16:50:06 +0530

    minor changes in 08

--- react/react-by-HC/08-context-api-1/src/components/Login.jsx
+++ react/react-by-HC/08-context-api-1/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
                     placeholder='username'
                     value={userName}
                     onChange={(e) => SetUserName(e.target.value)}
-                    className='m-3 placeholder:text-slate-400 bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm sm:max-w-xs '
+                    className='m-3 placeholder:text-slate-400 bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm sm:max-w-xs sm:w-60'
 
                 />
                 <input
@@ -30,12 +30,12 @@ const Login = () => {
                     placeholder='password'
                     value={password}
                     onChange={(e) => SetPassword(e.target.value)}
-                    className='outline-none  m-2  placeholder:text-slate-400 bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm '
+                    className='outline-none  m-2  placeholder:text-slate-400 bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1  sm:w-60'
                 />
 
                 <button
                     onClick={handleSubmit}
-                    className='text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
+                    className='text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2 sm:w-60 w-'
                 >Submit</button>
             </div>
         </div>
