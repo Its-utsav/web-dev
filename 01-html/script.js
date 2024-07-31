a8c11c198be1f9fd93b1259227855fdee333ea36
its-utsav <utsavdhimmar987@gmail.com>
2024-07-31 21:13:42 +0530

    html-1

---
+++ 01-html/script.js
@@ -0,0 +1,10 @@
+
+
+// let autoReload = setInterval(function () {
+//     window.location.reload();
+//     console.log('refresh ')
+// }, 2 * 1000)
+// function stopReload() {
+//     clearInterval(autoReload)
+//     console.log('stop ed');
+// }
